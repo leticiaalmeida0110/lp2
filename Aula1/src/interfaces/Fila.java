6cf8c739ea2dbd1964bace912f222389732fd486
Letícia Oliveira Almeida de Rezende <14976669610@MUZAMBINHO.EDU.BR>
2026-08-24 15:26:03 -0300

    lp

---
+++ Aula1/src/interfaces/Fila.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+public interface Fila {
+    public abstract void add(Integer valor);
+    public abstract Integer remove();
+    //public abstract Integer get(int index);
+    public abstract boolean isEmpty();
+    void mostra();
+
+
+    // add(valor: Imnteger): void
+    // remove(): Integer
+    // get(index: int): Integer
+    // isEmpy(): boolean
+
+}
